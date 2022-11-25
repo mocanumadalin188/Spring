f6b705c988c2a5751621d31a74318cd333590b60
madalin.mocanu <verdictus@users.noreply.github.com>
2022-11-25 11:29:33 +0200

    added live coding project

---
+++ ComponentsAndIoC/src/main/java/com/example/componentsandioc/ComponentsAndIoCApplication.java
@@ -0,0 +1,13 @@
+package com.example.componentsandioc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComponentsAndIoCApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComponentsAndIoCApplication.class, args);
+    }
+
+}
---
+++ ComponentsAndIoC/src/test/java/com/example/componentsandioc/ComponentsAndIoCApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.componentsandioc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComponentsAndIoCApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
